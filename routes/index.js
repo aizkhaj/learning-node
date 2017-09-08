cc83d43f2288e6bcd439094c492f13b16a1816b7
Aizaz Khaja <aizazkj@gmail.com>
2017-09-07 22:05:10 -0400

    progress commit.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers.js');
 
 // Do work here
-router.get('/', storeController.homePage);
+router.route('/')
+  .get(storeController.myMiddleware, storeController.homePage);
+
+router.route('/add')
+  .get(storeController.addStore)
+  .post(catchErrors(storeController.createStore));
 
 module.exports = router;
